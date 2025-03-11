b2bba938401db56d4f0b038f610261390daa8f57
alexander rosario <alexanderrosario@alexanders-mbp.mynetworksettings.com>
2025-03-11 10:38:44 -0400

    mock up

--- js/main.js
+++ js/main.js
@@ -13,6 +13,9 @@ const modalOpen = '[data-open]';
 const modalClose = '[data-close]';
 const isVisible = 'is-visible';
 
+const dataFilter = '[data-filter]';
+const portfolioData ='[data-item]';
+
 const root = document.documentElement;
 
 /* Theme */
@@ -20,6 +23,10 @@ const toggleTheme = document.querySelector(themeTab);
 const switcher = document.querySelectorAll(switcherBtn);
 const currentTheme = localStorage.getItem(theme);
 
+/*Portfolio*/
+const filterLink = document.querySelectorAll(dataFilter);
+const portfolioItems = document.querySelectorAll(portfolioData);
+const searchBox = document.querySelector('#search');
 /*Modal */
 const openModal = document.querySelectorAll(modalOpen);
 const closeModal = document.querySelectorAll(modalClose);
@@ -71,9 +78,9 @@ for(const elm of switcher){
 
   })
 }
-/* */
 
 
+//Modal
 
 for (const elm of openModal) {
   elm.addEventListener('click', function() {
@@ -83,9 +90,63 @@ for (const elm of openModal) {
 }
 for (const elm of closeModal) {
   elm.addEventListener('click', function() {
-    this.parentElement.parentElement.classList.remove(isVisible);
+    this.parentElement.parentElement.parentElement.classList.remove(isVisible);
    
   })
 }
 
 
+document.addEventListener('click', (e) =>{
+  if (e.target === document.querySelector('.modal.is-visible')) {
+    document.querySelector('.modal.is-visible').classList.remove(isVisible);
+  }
+})
+
+document.addEventListener('keyup', (e) =>{
+  if (e.key === 'Escape') {
+    document.querySelector('.modal.is-visible').classList.remove(isVisible);
+  }
+})
+
+
+
+
+searchBox.addEventListener('keyup' , (e) => {
+  const searchInput = e.target.value.toLowerCase().trim();
+  
+  portfolioItems.forEach((card) => {
+    if(card.dataset.item.includes(searchInput)){
+      card.style.display ='block';
+    } else {
+      card.style.display ='none';
+    }
+  })
+});
+
+
+for( const link of filterLink){
+
+  link.addEventListener('click', function (){
+    setActive(link, '.filter-link');
+    const filter =this.dataset.filter;
+    portfolioItems.forEach((card)=>{
+      if(filter === 'all'){
+        card.style.display ='block';
+      } else if (card.dataset.item ===filter) {
+        card.style.display = 'block';
+      } else {
+        card.style.display = 'none';
+      }
+    })
+  })
+
+}
+
+const elmsDisplayed = getComputedStyle(root).getPropertyValue('--marquee-elms-displayed');
+const marqueeContent = document.querySelector('ul.marquee-content');
+
+root.style.setProperty('--marquee-elms', marqueeContent.children.length);
+
+for( let i = 0; i < elmsDisplayed; i += 1) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
